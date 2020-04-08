fd717ece085630bd717e669b0a422fe9ad5ed561
Jessica K <jessikim807@gmail.com>
2020-04-08 16:49:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,11 @@ def new_hash
 end
 
 def my_hash
-  my_hash[:address]
+  { :favorite_color => "red"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+    # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
